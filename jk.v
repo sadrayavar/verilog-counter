a187c0719606cb097f0705771cdf80e0aeb5a290
mrtaha80 <106156263+mrtaha80@users.noreply.github.com>
2023-01-30 10:19:10 +0330

    fixed jk module

--- jk.v
+++ jk.v
@@ -1,11 +1,11 @@
-module  JK(j,k,clock,clear,preset,Q);
+module  JK(j,k,clock,clear,Q,Qbar);
   
-  input j,k,clock,clear,preset;
-  output Q;
+  input j,k,clock,clear;
+  output Q,Qbar;
 
   nand(temp1,Qbar,j,clock);
   nand(temp2,Q,k,clock);  
   nand(Qbar,Q,temp2,clear);
-  nand(Q,Qbar,temp1,preset);
+  nand(Q,Qbar,temp1,1);
   
 endmodule
