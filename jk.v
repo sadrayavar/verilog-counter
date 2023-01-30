1f64ea3b97104b96873d87cbead6e2b26465722f
sadrayavar <sadra.yavarzadeh@gmail.com>
2023-01-30 12:08:59 +0330

    move moved jkTest to jk file

--- jk.v
+++ jk.v
@@ -15,3 +15,23 @@ module  JK(j,k,clock,clear,Q);
     endcase
   end  else q=0;
 endmodule
+
+module jkTest (); 
+
+    reg j=0,k=0,clock=1,clear=0;
+    wire q,qBar;
+
+    JK test(j,k,clock,clear,q,qBar);
+
+    always 
+    begin
+        #23 clock=~clock;
+    end
+
+    always 
+    begin
+        #10 j=~j;
+        #5 k=~k;
+    end
+    
+endmodule
