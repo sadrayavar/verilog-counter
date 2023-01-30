607a6fc828c1667c76deb0c9e7c3bab79734a769
mrtaha80 <106156263+mrtaha80@users.noreply.github.com>
2023-01-30 12:07:26 +0330

    conver jk to dataflow level

--- jk.v
+++ jk.v
@@ -1,11 +1,17 @@
-module  JK(j,k,clock,clear,Q,Qbar);
-  
+module  JK(j,k,clock,clear,Q);
   input j,k,clock,clear;
-  output Q,Qbar;
+  output Q=0;
 
-  nand(temp1,Qbar,j,clock);
-  nand(temp2,Q,k,clock);  
-  nand(Qbar,Q,temp2,clear);
-  nand(Q,Qbar,temp1,1);
-  
+    req Q;
+
+  always @ (posedge clock, clear)
+  if (clear==1'b1)
+  begin
+    case ({j, k})
+    2'b00 : q=q;
+    2'b01 : q=0;
+    2'b10 : q=1;
+    2'b11 : q=~q;
+    endcase
+  end  else q=0;
 endmodule
